fc95eabfeafd9e870c368bbf12a258275a60d6ae
Vladimir Proskurin <v.proskurin@gmail.com>
2019-07-16 23:03:27 +0300

    init ver. 1.0

---
+++ WFArrowClockCtrl/ArrowClockCtrl.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Diagnostics;
+
+namespace WFArrowClockCtrl
+{
+    public partial class ArrowClockCtrl: UserControl
+    {
+        const int DEGREES_IN_MINUTE = 6;
+        const int DEGREES_IN_HOUR = 30;
+        SolidBrush _minBrush = new SolidBrush(Color.Black);     
+
+        [Category("Внешний вид")]  
+        public Color MinColor
+        {
+            set { _minBrush = new SolidBrush(value); }
+            get { return _minBrush.Color;  }
+        }
+
+        SolidBrush _hourBrush = new SolidBrush(Color.Black);
+        [Category("Внешний вид")]
+        public Color HourColor
+        {
+            set { _hourBrush = new SolidBrush(value); }
+            get { return _hourBrush.Color; }
+        }
+
+        SolidBrush _secBrush = new SolidBrush(Color.Red);
+        [Category("Внешний вид")]
+        public Color SecColor
+        {
+            set { _secBrush = new SolidBrush(value); }
+            get { return _secBrush.Color; }
+        }
+        public ArrowClockCtrl()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Resource1.system;
+        }
+
+
+        [Browsable(false), DesignerSerializationVisibility(
+             DesignerSerializationVisibility.Hidden)]
+        public override Image BackgroundImage { get; set; }
+
+
+        public enum BackImage
+        {
+            Cronometer, Diner, Flower, Modern, Square, System, Trad
+        }
+
+        BackImage _backPicture;
+        [Category("Внешний вид")]
+        public BackImage BackPicture
+        {
+            set
+            {
+                _backPicture = value;
+                switch (value)
+                {
+                    case BackImage.Cronometer: BackgroundImage = Resource1.cronometer; break;
+                    case BackImage.Diner: BackgroundImage = Resource1.diner; break;                       
+                    case BackImage.Flower: BackgroundImage = Resource1.flower; break;
+                    case BackImage.Modern: BackgroundImage = Resource1.modern; break;
+                    case BackImage.Square: BackgroundImage = Resource1.square; break;
+                    case BackImage.System: BackgroundImage = Resource1.system; break;
+                    case BackImage.Trad:BackgroundImage = Resource1.trad; break;                   
+                }
+            }
+
+            get
+            {
+                return _backPicture;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void ArrowClockCtrl_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.TranslateTransform(Size.Width / 2, Size.Height / 2);
+
+            Point[] points = new Point[] { new Point(-3, 0), new Point(0, -40), new Point(3, 0), new Point(0, 3) };
+            float angle = DateTime.Now.Minute * DEGREES_IN_MINUTE + DateTime.Now.Second / 10.0f;
+            DrawArrow(graphics, points, _minBrush, angle);
+
+            points = new Point[] { new Point(-3, 0), new Point(0, -25), new Point(3, 0), new Point(0, 3) };
+            angle = DateTime.Now.Hour * DEGREES_IN_HOUR + DateTime.Now.Minute / 2.0f;
+            DrawArrow(graphics, points, _hourBrush, angle);
+
+            points = new Point[] { new Point(-1, 0), new Point(0, -50), new Point(1, 0), new Point(0, 1) };
+            angle = DateTime.Now.Second * DEGREES_IN_MINUTE;
+            DrawArrow(graphics, points, _secBrush, angle);
+        }
+
+        private void DrawArrow(Graphics graphics, Point[] points, Brush brush, float angle)            
+        {
+            GraphicsPath arrow = new GraphicsPath();
+            arrow.StartFigure();        
+            arrow.AddPolygon(points);
+            Matrix matrix = new Matrix();         
+            matrix.Rotate(angle);
+            //Debug.WriteLine(DateTime.Now.Minute + ":" + DateTime.Now.Second + " angle: " + minAngle);
+            arrow.Transform(matrix);
+            graphics.FillPolygon(brush, arrow.PathPoints);
+        }
+    }
+}
---
+++ WFTestArrowClockCtrl/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WFTestArrowClockCtrl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arrowClockCtrl2 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl1 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl3 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl4 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl5 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl6 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.arrowClockCtrl7 = new WFArrowClockCtrl.ArrowClockCtrl();
+            this.SuspendLayout();
+            // 
+            // arrowClockCtrl2
+            // 
+            this.arrowClockCtrl2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl2.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Diner;
+            this.arrowClockCtrl2.HourColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl2.Location = new System.Drawing.Point(191, 22);
+            this.arrowClockCtrl2.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl2.MinColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl2.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl2.Name = "arrowClockCtrl2";
+            this.arrowClockCtrl2.SecColor = System.Drawing.Color.MistyRose;
+            this.arrowClockCtrl2.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl2.TabIndex = 1;
+            // 
+            // arrowClockCtrl1
+            // 
+            this.arrowClockCtrl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl1.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Cronometer;
+            this.arrowClockCtrl1.HourColor = System.Drawing.Color.WhiteSmoke;
+            this.arrowClockCtrl1.Location = new System.Drawing.Point(33, 22);
+            this.arrowClockCtrl1.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl1.MinColor = System.Drawing.Color.WhiteSmoke;
+            this.arrowClockCtrl1.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl1.Name = "arrowClockCtrl1";
+            this.arrowClockCtrl1.SecColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl1.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl1.TabIndex = 0;
+            // 
+            // arrowClockCtrl3
+            // 
+            this.arrowClockCtrl3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl3.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Flower;
+            this.arrowClockCtrl3.HourColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl3.Location = new System.Drawing.Point(33, 169);
+            this.arrowClockCtrl3.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl3.MinColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl3.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl3.Name = "arrowClockCtrl3";
+            this.arrowClockCtrl3.SecColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl3.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl3.TabIndex = 2;
+            // 
+            // arrowClockCtrl4
+            // 
+            this.arrowClockCtrl4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl4.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Modern;
+            this.arrowClockCtrl4.HourColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl4.Location = new System.Drawing.Point(373, 22);
+            this.arrowClockCtrl4.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl4.MinColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl4.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl4.Name = "arrowClockCtrl4";
+            this.arrowClockCtrl4.SecColor = System.Drawing.Color.DarkSlateGray;
+            this.arrowClockCtrl4.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl4.TabIndex = 3;
+            // 
+            // arrowClockCtrl5
+            // 
+            this.arrowClockCtrl5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl5.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Square;
+            this.arrowClockCtrl5.HourColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl5.Location = new System.Drawing.Point(373, 169);
+            this.arrowClockCtrl5.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl5.MinColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl5.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl5.Name = "arrowClockCtrl5";
+            this.arrowClockCtrl5.SecColor = System.Drawing.Color.DarkSlateGray;
+            this.arrowClockCtrl5.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl5.TabIndex = 4;
+            // 
+            // arrowClockCtrl6
+            // 
+            this.arrowClockCtrl6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl6.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.System;
+            this.arrowClockCtrl6.HourColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl6.Location = new System.Drawing.Point(191, 169);
+            this.arrowClockCtrl6.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl6.MinColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl6.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl6.Name = "arrowClockCtrl6";
+            this.arrowClockCtrl6.SecColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl6.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl6.TabIndex = 5;
+            // 
+            // arrowClockCtrl7
+            // 
+            this.arrowClockCtrl7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.arrowClockCtrl7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.arrowClockCtrl7.BackPicture = WFArrowClockCtrl.ArrowClockCtrl.BackImage.Trad;
+            this.arrowClockCtrl7.HourColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl7.Location = new System.Drawing.Point(33, 314);
+            this.arrowClockCtrl7.MaximumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl7.MinColor = System.Drawing.Color.Black;
+            this.arrowClockCtrl7.MinimumSize = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl7.Name = "arrowClockCtrl7";
+            this.arrowClockCtrl7.SecColor = System.Drawing.Color.Red;
+            this.arrowClockCtrl7.Size = new System.Drawing.Size(130, 130);
+            this.arrowClockCtrl7.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 456);
+            this.Controls.Add(this.arrowClockCtrl7);
+            this.Controls.Add(this.arrowClockCtrl6);
+            this.Controls.Add(this.arrowClockCtrl5);
+            this.Controls.Add(this.arrowClockCtrl4);
+            this.Controls.Add(this.arrowClockCtrl3);
+            this.Controls.Add(this.arrowClockCtrl2);
+            this.Controls.Add(this.arrowClockCtrl1);
+            this.Name = "Form1";
+            this.Text = "Test ArrowCloclCtrl";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl1;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl2;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl3;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl4;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl5;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl6;
+        private WFArrowClockCtrl.ArrowClockCtrl arrowClockCtrl7;
+    }
+}
+
